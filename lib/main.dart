79ac2d1e2bab426118aac3317d8c49583632b875
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-08 07:57:38 +0530

    Load environment variables added to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env"); // Load environment variables
+
   runApp(const MyApp());
 }
 
